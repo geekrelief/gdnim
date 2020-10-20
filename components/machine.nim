360ea57996cdb65edf50714fab5a052183009f63
geekrelief <geekrelief@gmail.com>
2020-10-19 20:36:33 -0700

    moved calls_watcher to watcher, add hot.nim with save reload macros

--- components/machine.nim
+++ components/machine.nim
@@ -3,15 +3,18 @@ import godotapi / node
 import strformat
 
 import msgpack4nim
+import hot
 
 gdobj Machine of Node:
 
+  var compName = "machine"
   var intVal = 4
-  var int2Val = 22
+  var int2Val = 11
   var strVal = "so far so good"
   var tickRate = 2.0
   var elapsedSeconds:float
 
+#[
   method enter_tree() =
     var cwatcher = self.get_node("/root/CallsWatcher") #case sensitive
     if not cwatcher.isNil:
@@ -33,6 +36,13 @@ gdobj Machine of Node:
     var b = MsgStream.init()
     b.pack(self.intVal)
     b.data
+]#
+  method enter_tree() =
+    print "Machine enter_tree"
+    load(self.intVal)
+
+  proc reload():string {.gdExport.} =
+    save(self.intVal)
 
   method process(delta:float64) =
     self.elapsedSeconds += delta
---
+++ hot.nim
@@ -0,0 +1,82 @@
+import macros
+import msgpack4nim
+export msgpack4nim
+
+proc `^`*(s:string):NimNode {.inline.} =
+  ident(s)
+
+macro save*(args: varargs[typed]):untyped =
+  #[
+    #save(self.i, self.f, self.s) produces
+    self.queue_free()
+    var b = MsgStream.init()
+    b.pack(self.i)
+    b.pack(self.f)
+    b.pack(self.s)
+    b.data
+  ]#
+  var stmts = newStmtList()
+  stmts.add newCall(newDotExpr(^"self", ^"queue_free"))
+  stmts.add newVarStmt(^"b", newCall(newDotExpr(^"MsgStream", ^"init")))
+  for arg in args:
+    stmts.add newCall(newDotExpr(^"b", ^"pack"),
+      newDotExpr(^"self", ^(arg[1].repr))
+    )
+  stmts.add newDotExpr(^"b", ^"data")
+  result = stmts
+
+macro load*(args: varargs[typed]):untyped =
+  #[
+    load(data, self.i)
+    var w = self.get_node("/root/Watcher")
+    if w.isNil:
+      raise newException(Defect, "Watcher not found")
+
+    var data = w.call("register_component",
+      self.compName.toVariant,
+      ($self.get_parent().get_path()).toVariant,
+      ($self.get_path()).toVariant).asString
+    if data.len == 0: return
+    var b = MsgStream.init(data)
+    var i:int
+    b.unpack(i)
+    self.intVal = i
+  ]#
+  var stmts = newStmtList()
+  stmts.add nnkVarSection.newTree(
+    nnkIdentDefs.newTree(^"w", newEmptyNode(), newCall(newDotExpr(^"self", ^"get_node"), newLit("/root/Watcher")))
+  )
+
+  stmts.add newIfStmt(
+    (newDotExpr(^"w", ^"isNil"), newStmtlist(nnkRaiseStmt.newTree(newCall(^"newException", ^"Defect", newLit("Watcher not found")))))
+  )
+  stmts.add nnkVarSection.newTree(
+    nnkIdentDefs.newTree(^"data", newEmptyNode(), newDotExpr(
+      newCall(
+        newDotExpr(^"w", ^"call"),
+        newLit("register_component"),
+        newDotExpr(newDotExpr(^"self", ^"compName"), ^"toVariant"),
+        newDotExpr(nnkPar.newTree(nnkPrefix.newTree(^"$", newCall(newDotExpr(newCall(newDotExpr(^"self", ^"get_parent")),^"get_path")))), ^"toVariant"),
+        newDotExpr(nnkPar.newTree(nnkPrefix.newTree(^"$", newCall(newDotExpr(^"self", ^"get_path")))), ^"toVariant")
+      ),
+      ^"asString")
+    )
+  )
+
+  stmts.add newIfStmt(
+    (nnkInfix.newTree(^"==", newDotExpr(^"data", ^"len"), newLit(0)),
+      newStmtList(nnkReturnStmt.newTree(newEmptyNode())))
+  )
+
+  stmts.add newVarStmt(^"b", newCall(newDotExpr(^"MsgStream", ^"init"), ^"data"))
+
+  for aprop in args:
+    var prop = ^($aprop[1])
+    var propType = ^($getType(aprop[1]))
+    stmts.add(
+      nnkVarSection.newTree( nnkIdentDefs.newTree(prop, propType, newEmptyNode())),
+      newCall(newDotExpr(^"b", ^"unpack"), prop),
+      newAssignment(newDotExpr(^"self", prop), prop)
+    )
+  #echo stmts.astGenRepr
+  result = stmts
