2d10e495a655de9c93ad2c68e900d29a74d1107e
geekrelief <geekrelief@gmail.com>
2020-11-26 08:15:49 -0800

    add signal declaration keyword for godot-nim

--- components/test_comp.nim
+++ components/test_comp.nim
@@ -3,6 +3,7 @@ import godotapi / [node, label]
 import hot
 import strformat
 import tables
+import macros
 
 gdobj TestComp of Label:
   var tick:int
@@ -11,9 +12,19 @@ gdobj TestComp of Label:
   var anInt:int = 222
   var aString1:string = "update"
 
+  signal test_sig(a_bool:bool, a_int8:int8, a_string:string)
+
   method enter_tree() =
     self.text = "TestComp enter_tree"
     register(test_comp)?.load(self.tick, self.aString1)
+
+  method ready() =
+    discard self.connect("test_sig", self, "on_test_sig")
+    self.emitSignal("test_sig", true.toVariant, 123.toVariant, "hello".toVariant)
+    self.emitSignal("test_sig", false.toVariant, (-128).toVariant, "world".toVariant)
+
+  proc on_test_sig(a_bool:bool, a_int8:int8, a_string:string) {.gdExport.} =
+    print &"got test_sig {a_bool = } {a_int8 = } {a_string = }"
 
   proc reload():seq[byte] {.gdExport.} =
     self.queue_free()
