4f5ef1db04c463f222694090240a43e868146a3e
geekrelief <geekrelief@gmail.com>
2020-12-25 23:05:48 -0800

    Added async handling for signals, with return val where return val Future[T] where T can be void, a simple type, a core godot type or tuple of simple and core godot types

--- components/sprite_comp.nim
+++ components/sprite_comp.nim
@@ -1,25 +1,38 @@
 import godot
-import godotapi / [sprite, input_event_mouse_button, scene_tree]
+import godotapi / [sprite, input_event_mouse_button, scene_tree, timer]
 import hot
 import strformat
-import math
+import math, asyncdispatch
 
 gdobj SpriteComp of Sprite:
   var startPos:Vector2 = vec2(100.0, 100.0)
   var radius:float = 50.0
   var speed:float = 0.2
   var elapsedTime:float
+  var timer:Timer
 
-  signal clicked(button_idx:int, shape_idx:int)
+  signal click()
+  signal bclick(button_idx:int)
+  signal bsclick(button_idx:int, shape_idx:int)
 
   method enter_tree() =
     discard register(sprite_comp)
     self.position = self.startPos
+    self.timer = gdnew[Timer]()
+    self.timer.one_shot = true
+    self.add_child(self.timer)
 
   method ready() =
     var area2D = self.get_node("Area2D")
     discard area2D.connect("input_event", self, "on_area2d_input_event")
-    discard self.connect("clicked", self, "on_clicked")
+    discard self.connect("bsclick", self, "on_bsclick")
+
+    registerFrameCallback(
+      proc() =
+        if hasPendingOperations():
+          poll(0)
+    )
+    asyncCheck self.fireTimer()
 
   proc reload():seq[byte] {.gdExport.} =
     self.queue_free()
@@ -27,13 +40,72 @@ gdobj SpriteComp of Sprite:
   proc on_area2d_input_event(viewport:Node, event:InputEvent, shape_idx:int) {.gdExport.} =
     ifis(event, InputEventMouseButton):
       if it.pressed:
-        self.emit_signal("clicked", it.button_index.toVariant, shape_idx.toVariant)
+        self.emit_signal("click")
+        self.emit_signal("bclick", it.button_index.toVariant)
+        self.emit_signal("bsclick", it.button_index.toVariant, shape_idx.toVariant)
         self.get_tree().set_input_as_handled()
 
-  proc on_clicked(button_idx:int, shape_idx:int) {.gdExport.} =
-    print &"{button_idx = } clicked {shape_idx = }"
+  proc on_bsclick(button_idx:int, shape_idx:int) {.gdExport.} =
+    print &"bsclick {button_idx = } {shape_idx = }"
 
   method process(delta:float64) =
     self.elapsedTime += delta
     var angle = self.elapsedTime * TAU * self.speed
     self.position = self.startPos + self.radius * vec2(cos(angle), sin(angle))
+
+  proc rotate45() =
+    self.rotation_degrees = self.rotation_degrees + 45
+
+  proc fireTimer() {.async.} =
+    await on_signal(self.get_tree().createTimer(1), "timeout")
+    print "timeout sceneTree"
+
+    var val = await on_signal(self, "bclick", int)
+    print &"bclick {val = }"
+    self.rotate45()
+
+    var vals = await on_signal(self, "bsclick", tuple[button_idx:int, shape_idx:int])
+    print &"bsclick {vals.button_idx = } {vals.shape_idx = }"
+    self.rotate45()
+
+    for c in "A LONG TIME AGO IN A GALAXY FAR, FAR AWAY":
+      self.timer.start(0.05)
+      await onSignal(self.timer, "timeout")
+      print c
+
+    var fast = false
+    if fast: #nnkIfStmt
+      self.timer.start(0.15)
+      await on_signal(self.timer, "timeout")
+      print "fast timeout"
+    else: #nnkElseStmt
+      self.timer.start(1.15)
+      await on_signal(self.timer, "timeout")
+      print "slow timeout"
+
+    print "waiting for timeout 4 OR click"
+    var f1 = on_signal(self.get_tree().createTimer(4), "timeout")
+    var f2 = on_signal(self, "click")
+    await f1 or f2
+    if f1.finished:
+      print "timeout or click: timeout"
+    if f2.finished:
+      print "timeout or click: click"
+      self.rotate45()
+
+    print "waiting for timeout 2 seconds AND click"
+    await on_signal(self.get_tree().createTimer(2), "timeout") and on_signal(self, "click")
+    print "timeout scene tree timer and click"
+    self.rotate45()
+
+    var c = 'b'
+    case c:
+      of 'a':
+        print "case a, waiting for timeout"
+        await on_signal(self.get_tree().createTimer(4), "timeout")
+        print "tree timer timeout"
+      else:
+        print "case b, waiting for click"
+        await on_signal(self, "click")
+        print "click"
+        self.rotate45()
--- deps/godot/nim/godotmacros.nim
+++ deps/godot/nim/godotmacros.nim
@@ -1,6 +1,6 @@
 # Copyright 2018 Xored Software, Inc.
 
-import macros, tables, typetraits, strutils, sets, sequtils, options, algorithm
+import macros, strutils, sets, options
 import godotinternal, internal/godotvariants
 import godotnim, core/variants
 
@@ -91,6 +91,17 @@ iterator pragmas(node: NimNode):
       yield (node[index][0].strVal, node[index][1], index)
     elif node[index].kind == nnkIdent:
       yield (node[index].strVal, nil, index)
+
+proc hasPragma(statement: NimNode, pname: string): bool =
+  if not (RoutineNodes.contains(statement.kind) or
+          statement.kind == nnkPragmaExpr):
+    return false
+
+  var pragmas = if RoutineNodes.contains(statement.kind): statement.pragma()
+                else: statement[1]
+  for ident, val, i in pragmas(pragmas):
+    if ident == pname:
+      return true
 
 proc removePragmaNode(statement: NimNode,
                       pname: string): NimNode {.compileTime.} =
@@ -210,6 +221,138 @@ proc parseSignal(sig: NimNode): SignalDecl =
       else:
         parseError(sig, errorMsg)
 
+proc parseOnSignalCall(onSignalId:NimNode, onSignalPostfix:string, onSigStmt:NimNode): (VarDecl, MethodDecl, MethodDecl) =
+  # future
+  let futureId = ident("f" & onSignalPostfix)
+  var futureType = newNimNode(nnkBracketExpr)
+  futureType.add ident("Future")
+  var futureDef:NimNode
+
+  let callbackId = ident("cb" & onSignalPostfix)
+  var callbackDef = quote do:
+      proc `callbackId`() {.gdExport.} =
+        self.`futureId`.complete()
+
+  let formatErrorMsg = "on_signal must have this format: on_signal(target:Object, signalName:string, [void|T])"
+  if onSigStmt.len == 3:
+    futureType.add ident("void")
+    futureDef = quote do:
+      var `futureId`:`futureType`
+  elif onSigStmt.len == 4:
+    case onSigStmt[3].kind:
+      of nnkTupleTy:
+        futureType.add newNimNode(nnkTupleTy) #Future[tuple[arg1:type1, arg2:type2]]
+
+        callbackDef[^1][0].add newNimNode(nnkPar) #self.f.complete(())
+        var fcompleteCallArgs = callbackDef[^1][0][^1]
+
+        for arg in onSigStmt[3]:
+          var identDefs = newIdentDefs(arg[0], arg[1])
+          futureType[1].add identDefs
+          callbackDef[3].add identDefs
+          fcompleteCallArgs.add arg[0]
+
+        futureDef = quote do:
+          var `futureId`:`futureType`
+      of nnkIdent:
+        var fT = onSigStmt[3]
+        futureType.add fT
+
+        var callbackArg = ident("arg")
+        callbackDef[3].add newIdentDefs(callbackArg, fT)
+        callbackDef[6][0].add callbackArg
+
+        echo callbackDef.repr()
+
+        futureDef = quote do:
+          var `futureId`:`futureType`
+      else:
+        parseError(onSigStmt, formatErrorMsg)
+  else:
+    parseError(onSigStmt, formatErrorMsg)
+
+  # connect
+  let callbackIdLit = callbackId.toStrLit()
+  var newFutureCall = newNimNode(nnkCall)
+  newFutureCall.add newNimNode(nnkBracketExpr)
+  newFutureCall.add onSignalId.toStrLit()
+  var newFutureType = newFutureCall[0]
+  newFutureType.add ident("newFuture")
+  newFutureType.add futureType[1..^1]
+
+  let onSignalDef = quote do:
+    proc `onSignalId`(target:Object, signalName:string):`futureType` =
+      self.`futureId` = `newFutureCall`
+      if not target.is_connected(signalName, self, `callbackIdLit`):
+        discard target.connect(signalName, self, `callbackIdLit`, flags = CONNECT_ONESHOT)
+      self.`futureId`
+
+  result = (parseVarSection(futureDef)[0], parseMethod(callbackDef), parseMethod(onSignalDef))
+
+# recurse through statement looking for onSignal
+# returns modified statement, futures, and callback / onSignal declarations
+proc recurseOnSignalCalls(methodName:string, signalCallIds:var HashSet[string], nnode:NimNode): (NimNode, seq[VarDecl], seq[MethodDecl]) =
+  var futureDecls:seq[VarDecl]
+  var methodDecls:seq[MethodDecl]
+  var newNNode:NimNode = nnode
+
+  case nnode.kind
+    of nnkCall:
+      if nnode[0] == ident("onSignal"):
+        #generate the future, callback and onSignal proc for the signal
+        var target = nnode[1]
+        var targetName = if target.kind != nnkIdent: genSym().toStrLit.strVal[1..^1] else: target.repr
+        targetName = targetName.toLower
+        var signalName = nnode[2].strVal.toLower
+        var onSignalPostfix = "_" & methodName & "_" & targetName & "_" & signalName
+        let onSignalId = ident("on_signal" & onSignalPostfix)
+        newNNode = (quote do:
+          self.`onSignalId`(`target`, `signalName`))
+
+        if not signalCallIds.containsOrIncl(onSignalPostfix):
+          let (fdecl, callbackDecl, onSignalDecl) = parseOnSignalCall(onSignalId, onSignalPostfix, nnode)
+          futureDecls.add fdecl
+          methodDecls.add @[callbackDecl, onSignalDecl]
+      else:
+        for i in 1..<nnode.len:
+          var (newArg, fdecls, mdecls) = recurseOnSignalCalls(methodName, signalCallIds, nnode[i])
+          newNNode[i] = newArg
+          futureDecls.add fdecls
+          methodDecls.add mdecls
+    else:
+      if nnode.len > 0:
+        newNNode = newNimNode(nnode.kind)
+        for child in nnode:
+          var (newStatement, fdecls, mdecls) = recurseOnSignalCalls(methodName, signalCallIds, child)
+          newNNode.add newStatement
+          futureDecls.add fdecls
+          methodDecls.add mdecls
+
+  result = (newNNode, futureDecls, methodDecls)
+
+proc parseAsyncMethod(meth:NimNode): (seq[VarDecl], seq[MethodDecl]) =
+  var signalCallIds:HashSet[string]
+  var (newNNode, futures, methods) = recurseOnSignalCalls($meth[0].basename.strVal.toLower, signalCallIds, meth[^1])
+
+  meth[^1] = newNNode
+  let isGdExport = removePragma(meth, "gdExport")
+  let isNoGodot = (meth.kind != nnkMethodDef and not isGdExport) or
+                  removePragma(meth, "noGdExport")
+  var md = MethodDecl(
+    name: $meth[0].basename,
+    args: newSeq[VarDecl](),
+    returnType: meth[3][0],
+    isVirtual: meth.kind == nnkMethodDef,
+    isNoGodot: isNoGodot,
+    nimNode: meth
+  )
+  for i in 1..<meth[3].len:
+    var identDefs = meth[3][i]
+    md.args.add(identDefsToVarDecls(identDefs))
+  methods.add md
+
+  result = (futures, methods)
+
 proc parseType(ast: NimNode): ObjectDecl =
   let definition = ast[0]
   let body = ast[^1]
@@ -238,8 +381,13 @@ proc parseType(ast: NimNode): ObjectDecl =
         let varSection = parseVarSection(statement)
         result.fields.add(varSection)
       of nnkProcDef, nnkMethodDef:
-        let meth = parseMethod(statement)
-        result.methods.add(meth)
+        if hasPragma(statement, "async"):
+          let (futures, handlers) = parseAsyncMethod(statement)
+          result.fields.add(futures)
+          result.methods.add(handlers)
+        else:
+          let meth = parseMethod(statement)
+          result.methods.add(meth)
       of nnkCommand:
         if statement[0].strVal == "signal":
           let sig = parseSignal(statement)
@@ -722,7 +870,10 @@ macro gdobj*(ast: varargs[untyped]): untyped =
   ## Generates Godot type. Self-documenting example:
   ##
   ## .. code-block:: nim
-  ##   import godot, node
+  ##   import godot,
+  ##   import godotapi / [node, scene_tree]
+  ##   import asyncdispatch
+  ##   ## For async, signal handling
   ##
   ##   gdobj MyObj of Node:
   ##     var myField: int
@@ -744,17 +895,47 @@ macro gdobj*(ast: varargs[untyped]): untyped =
   ##       ## (in this case ``_ready``)
   ##       print("I am ready! myString is: " & self.myString)
   ##
-  ##       ## connect to the signal and then emit it
   ##       discard self.connect("my_signal", self, "on_my_signal")
+  ##       ## Connect to the my_signal and then emit it
   ##       self.emit_signal("my_signal", 123.toVariant, "hello godot".toVariant)
+  ##
+  ##       ## Another way to handle signals is via async procs
+  ##       registerFrameCallback(
+  ##         proc() =
+  ##           if hasPendingOperations():
+  ##             poll(0)
+  ##         )
+  ##       )
+  ##       ## Setup Future polling for async procs
+  ##
+  ##       asyncCheck self.gameLogic()
+  ##       ## Start our async proc
+  ##       self.emit_signal("my_signal", 456.toVariant, "sending to async".toVariant)
   ##
   ##     proc myProc*() {.gdExport.} =
   ##       ## Exported to Godot as ``my_proc``
   ##       print("myProc is called! Incrementing myField.")
   ##       inc self.myField
   ##
-  ##     proc on_my_signal(amount:int, message:string) {.gdExport.} =
+  ##     proc onMySignal(amount:int, message:string) {.gdExport.} =
   ##       print("received my_signal " & amount & " " & message)
+  ##
+  ##     proc gameLogic() {.async.} =
+  ##       ## Processing is done on an {.async.} pragma proc
+  ##       ## to handle any "on_signal" calls.
+  ##       ## "on_signal" returns a Future[T]
+  ##       ## where T is the type passed in as the third argument
+  ##
+  ##       print "wait 1 second"
+  ##       await on_signal(self.get_tree().createTimer(1), "timeout")
+  ##       ## Basic signal handling with no arguments: Future[void]
+  ##       print "timeout sceneTree"
+  ##
+  ##       var vals = await on_signal(self, "my_signal",
+  ##                                    tuple[amount:int, message:string])
+  ##       ## To handle signals with arguments pass in a tuple type
+  ##       print("got amount:" & vals.amount & " message:" & vals.message)
+  ##       ## Here we read the values from the Future[tuple[amount:int, message:string]]
   ##
   ## If parent type is omitted, the type is inherited from ``Object``.
   ##
